ec2c23301ed685c5966997bbd90ffe7d5892984a
thegm445 <110239666+thegm445@users.noreply.github.com>
2023-06-14 00:41:45 +0000

    [feature]: addUser, classes user e BD add

--- ATM_caseiro/ATM.py
+++ ATM_caseiro/ATM.py
@@ -1,23 +1,84 @@
 import json
 
+class User():
+    def __init__(self, nome, cpf, endereco, senha):
+        self.nome = nome
+        self.cpf = cpf
+        self.endereco = endereco
+        self.senha = senha
+
+
 class Autentication():
 
     def __init__(self):
         self.cpf = ""
         self.senha = ""
+        self.usuarioAutenticado = False
+        self.gerenteAutenticado =  False
 
     def fazer_login(self):
-        with open("ATM_caseiro/users.json", "r") as arquivo:
+        foundClient, gerente = False
+        with open("Projeto4/ATM_caseiro/users.json", "r") as arquivo:
             dados = json.load(arquivo)
         
         self.cpf = str(input("CPF: "))
         self.senha = str(input("Senha: "))
 
-        for usuario in dados: 
+
+        for usuario in dados:
             if usuario['cpf'] == self.cpf and usuario['senha'] == self.senha:
-                print('Login bem-sucedido!')
-                return
+
+                if usuario['cpf'] == '00':
+                    gerente = True
+                    foundClient = True
+                    break
+
+                foundClient = True
+                break
+
+        if foundClient and not gerente:
+            self.usuarioAutenticado = True
+            print('Login bem-sucedido!')
+
+        elif gerente:
+            self.gerenteAutenticado = True
+            print('Sudo Su bem-sucedido!')
+
+        else:
             print("Credenciais inválidas")
+
+
+
+
+class BancoDeDados():
+    def __init__(self):
+        pass
+
+    def addUser():
+        wh = "Projeto4/ATM_caseiro/users.json"
+        usersList = []
+
+        with open("Projeto4/ATM_caseiro/users.json") as js:
+            usersList = json.load(js)
+
+        nome = input("\nPor favor digite seu nome: ")
+        cpf = input("\nPor favor digite seu CPF: ")
+        endereco = input("\nPor favor digite seu endereco: ")
+        senha = input("\nPor favor digite sua senha: ")
+
+        newUser = User(nome,cpf,endereco,senha)
+        convertUser = vars(newUser)
+
+
+        usersList.append(convertUser)
+
+        with open(wh,"w") as where:
+            json.dump(usersList,where,indent=4)
+        print("\n->\tUsuário Salvo!\n")
+
+    
+
+
 
 class MenuCliente():
     def __init__(self):
@@ -26,7 +87,7 @@ class MenuCliente():
 
     def exibir_menu():
         print("\n===== ATM CASEIRO =====")
-        print("1. Extrato\n")
+        print("1. hyhyhy\n")
         print("2. Saque\n")
         print("3. Depósito\n")
         print("4. Realizar Pagamento\n")
