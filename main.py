780baecf508f1847cde84d76f8c39925a99c957a
thegm445 <110239666+thegm445@users.noreply.github.com>
2023-06-14 02:01:24 +0000

    [features]: login como cliente e gerente distintos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ATM_caseiro.ATM import Autentication
+from ATM_caseiro.ATM import Autentication,MenuCliente,MenuGerente
 
 
 def workspace():
@@ -6,5 +6,14 @@ def workspace():
     login = Autentication()
 
     login.fazer_login()
+
+    
+    if login.gerenteAutenticado:
+        menuGerente = MenuGerente()
+        menuGerente.loop()
+
+    elif login.usuarioAutenticado:
+        menuCliente = MenuCliente()
+        menuCliente.loop()
 
 workspace()
